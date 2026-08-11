8f0852c749b9b76d7e9bf8905d3aab18fc57c008
Kumaranuj75 <759sainikschoolambikapur@gmail.com>
2026-08-11 18:54:39 +0530

    Changes

--- 05.3Sum.cpp
+++ 05.3Sum.cpp
@@ -42,3 +42,22 @@ public:
         if(i>0 && nums[i] == nums[i-1]) continue;
 
         int j = i+1, k = n-1;
+
+         while(j < k){
+            int sum = nums[i] + nums[j] + nums[k];
+            if(sum < 0){
+                j++;
+            } else if(sum > 0) {
+                k--;
+            } else {
+                ans.push_back({nums[i], nums[j], nums[k]});
+                j++; k--;
+
+                while(j < k && nums[j] == nums[j-1]) j++;
+            }
+        }
+      }
+      return ans;
+     
+    }
+};
