38265b3e0083a10c5ea2d02353449556d1da9fb9
Kumaranuj75 <759sainikschoolambikapur@gmail.com>
2026-08-20 20:35:22 +0530

    changes

--- 08moveZeros.cpp
+++ 08moveZeros.cpp
@@ -17,3 +17,21 @@
 
 
 
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        
+        int nonZero = 0;
+
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i] != 0){
+                swap(nums[i], nums[nonZero]);
+                nonZero++;
+            }
+        }
+    }
+};
