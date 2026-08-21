d29978c0f762cfc52c720912a5e6d16c1da1ee09
Kumaranuj75 <759sainikschoolambikapur@gmail.com>
2026-08-21 23:25:41 +0530

    Changes

--- 09maxConsecutive.cpp
+++ 09maxConsecutive.cpp
@@ -15,4 +15,19 @@
 // Output: 2
 
 
+#include<iostream>
+#include<vector>
+using namespace std;
 
+int findMaxConsecutiveOnes(vector<int>& nums) {
+    int count = 0, maxCount = 0;
+    for(int num : nums) {
+        if(num == 1) {
+            count++;
+            maxCount = max(maxCount, count);
+        } else {
+            count = 0;
+        }
+    }
+    return maxCount;
+}
