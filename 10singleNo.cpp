a5b5951c3c73870e34538955aa5e764a5b76a8d8
Kumaranuj75 <759sainikschoolambikapur@gmail.com>
2026-08-22 19:53:00 +0530

    changes

--- 10singleNo.cpp
+++ 10singleNo.cpp
@@ -26,4 +26,17 @@
 // Output: 1
 
 
+#include<iostream>
+#include<vector>
+using namespace std;
 
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ans = 0;
+        for(int i=0; i<nums.size(); i++){
+            ans ^= nums[i];
+        }
+        return ans;
+    }
+};
