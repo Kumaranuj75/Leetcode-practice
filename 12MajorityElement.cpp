cb4532e52df916148c3dc8bd031e43d6b37d57a1
Kumaranuj75 <759sainikschoolambikapur@gmail.com>
2026-09-05 01:01:52 +0530

    Changes

--- 12MajorityElement.cpp
+++ 12MajorityElement.cpp
@@ -15,3 +15,28 @@
 
 // Input: nums = [2,2,1,1,1,2,2]
 // Output: 2
+
+
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int freq = 0, ans = 0;
+
+        for(int i=0; i<nums.size(); i++){
+            if(freq == 0){
+                ans = nums[i];
+            }
+            if(ans == nums[i]){
+                freq++;
+            } else{
+                freq--;
+            }
+        }
+        return ans;
+    }
+};
