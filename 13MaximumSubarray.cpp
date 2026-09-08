cb63b8aba7faaff0d3483f2148d1cbc1d2044986
Kumaranuj75 <759sainikschoolambikapur@gmail.com>
2026-09-09 01:18:49 +0530

    changes

--- 13MaximumSubarray.cpp
+++ 13MaximumSubarray.cpp
@@ -24,4 +24,24 @@
 // Explanation: The subarray [5,4,-1,7,8] has the largest sum 23.
 
 
+#include<iostream>
+#include<vector>
+using namespace std;
 
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int currSum = 0, maxSum = INT_MIN;
+
+        for(int i = 0; i<nums.size(); i++){
+            currSum = currSum + nums[i];
+            maxSum = max(currSum, maxSum);
+
+            if(currSum < 0){
+                currSum = 0;
+            }
+        }
+        return maxSum;
+    }
+};
